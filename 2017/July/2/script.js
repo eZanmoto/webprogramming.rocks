6bab92f897e45bee83c4e03eaed076190d674c7b
Sean Kelleher <ezanmoto@gmail.com>
2017-07-07 15:03:21 +0100

    Add files via upload

---
+++ 2017/July/2/script.js
@@ -0,0 +1,10 @@
+
+$("#slowy").click(function () {
+alert("Cute but DEADLY");
+alert("Hello, world! €€€");
+
+});
+
+$("button").click(function () {
+alert("Hello "+$("#text").val()+"! How are you?");
+});
