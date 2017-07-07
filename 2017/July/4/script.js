33f74686f1dce9c22d6c9d8a9613fe3e6dac39e6
Sean Kelleher <ezanmoto@gmail.com>
2017-07-07 15:06:35 +0100

    Add files via upload

---
+++ 2017/July/4/script.js
@@ -0,0 +1,10 @@
+$("button").click(function () {
+
+
+
+
+alert("i am a dancer it is  one of my favourite things to do i dance at home i dance with KSM Kerry School Of MusicI am not that good but i still love dancing also really love gymnastics so whenever i make up a danc at home i like to add gymnastics to it")
+});
+
+
+button
