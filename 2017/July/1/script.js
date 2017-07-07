2e579dace6f5c6da0a6583a27e976c905e966676
Sean Kelleher <ezanmoto@gmail.com>
2017-07-07 14:59:25 +0100

    Add files via upload

---
+++ 2017/July/1/script.js
@@ -0,0 +1,24 @@
+$("button").click(function () {
+alert("Hello "+$("#text").val()+"! How are you?");
+});
+
+$("button").click(function () {
+alert("Mr. Teddy Gason farted");
+});
+
+$("#Teddy").click(function () {
+alert("OK!");
+});
+
+$("#Teddy").click(function () {
+alert("Good!");
+});
+
+$("#Teddy").click(function () {
+alert("What's your name?");
+});
+
+$("#Car").click(function () {
+$("p").toggle();
+});
+
