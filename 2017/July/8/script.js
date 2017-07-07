437da439a892355f9db18752b4bc4ef262e3481a
Sean Kelleher <ezanmoto@gmail.com>
2017-07-07 15:26:15 +0100

    Add files via upload

---
+++ 2017/July/8/script.js
@@ -0,0 +1,10 @@
+
+$("button").click(function () {
+
+alert("Hello World! Hello Harry Potter fans! Hello "+$("#text").val()+"! This is my website!");
+
+});
+
+$("#hg").click(function () {
+alert("Hermione's Homework Help! Get Hermione's Homewok Help today! Just tell Hermione that you want help with your homework, and she will help you! (She will not do your homework. She will HELP you.)")
+});
