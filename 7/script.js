e193af10ed685d325139f7b17f79dac0b98b5315
Sean Kelleher <ezanmoto@gmail.com>
2017-07-07 15:12:50 +0100

    Add files via upload

---
+++ 7/script.js
@@ -0,0 +1,30 @@
+$("#puppy").click(function () {
+alert("Hi I'm doggy I was born only 6 hours ago. I am the creaters assistint HELLOOOOOOOO!!!!");
+});
+
+
+$("#doge").click(function () {
+alert("Hi I'm bob i'm doggys new best friend we met 10mins ago i'm 2 or in dog years i'm 8");
+});
+
+
+
+$("button").click(function () {
+alert("hi "+$("#text").val()+" ! how are you?");
+});
+
+
+
+
+
+$("#cena").click(function () {
+alert("You can't see me");
+});
+
+
+
+
+
+
+
+
