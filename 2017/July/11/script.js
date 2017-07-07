ab000ff8708045132a4462ab88f315f8044d38f1
Sean Kelleher <ezanmoto@gmail.com>
2017-07-07 15:21:24 +0100

    Add files via upload

---
+++ 2017/July/11/script.js
@@ -0,0 +1,14 @@
+$("#kids").click(function() {
+alert("Strawberry,Lemon,Cherry,Pineapple,Watermelon,Grape,Banana and...TOMATO")
+});
+
+
+$("button").click(function () {
+
+alert("Hi Everyone:)!");
+
+});
+
+$("button").click(function(){
+alert("Hi "+$("#text").val()+" how are you guys doing?"); 
+});
